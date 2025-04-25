8ebe2273cfaa8e68e26c22cfa3935eba0600ce06
Aoamos <67606269+Aoamos@users.noreply.github.com>
2025-04-25 21:08:58 +0100

    Add function to get random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+const getComputerChoice = () => {
+  const randonNumber = Math.random();
+  if (randonNumber < 1 / 3) {
+    return "rock";
+  } else if (randonNumber < 2 / 3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
+
+console.log(getComputerChoice());
